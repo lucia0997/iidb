81fd9c819430a31dd06ff00fcefed30cd86aee79
Lucía Rodríguez <lucia0997@gmail.com>
2025-12-12 14:51:04 +0100

    urls actualizadas con las nuevas vistas de las tablas

--- backend/apps/iidb/urls.py
+++ backend/apps/iidb/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views.plant_programme import PlantProgrammeViewSet, PlantProgrammeRowDetailView
 from .views.technology import TechnologyViewSet, TechnologyRowDetailView
+from .views.strategy import StrategyViewSet, StrategyRowDetailView
+from .views.process import ProcessViewSet, ProcessRowDetailView
+from .views.project import ProjectViewSet, ProjectRowDetailView
 
 router = DefaultRouter()
 router.register(r"plants-programmes", PlantProgrammeViewSet, basename="plants-programmes")
 router.register(r"technologies", TechnologyViewSet, basename="technologies")
+router.register(r"strategies", StrategyViewSet, basename="strategies")
+router.register(r"processes", ProcessViewSet, basename="processes")
+router.register(r"projects", ProjectViewSet, basename="projects")
 
 urlpatterns = [
     path(
@@ -17,6 +23,21 @@ urlpatterns = [
         "technologies/rows/<int:pk>/",
         TechnologyRowDetailView.as_view(),
         name="technologies-row-detail"
+    ),
+    path(
+        "strategies/rows/<int:pk>/",
+        StrategyRowDetailView.as_view(),
+        name="strategies-row-detail"
+    ),
+    path(
+        "processes/rows/<int:pk>/",
+        ProcessRowDetailView.as_view(),
+        name="processes-row-detail"
+    ),
+    path(
+        "projects/rows/<int:pk>/",
+        ProjectRowDetailView.as_view(),
+        name="projects-row-detail"
     )
 ]
 
